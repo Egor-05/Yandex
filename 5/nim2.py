97fc61e40a1b385465e68b3c63d621c1d0d7426f
egor <e.kulpin@mail.ru>
2019-10-25 21:03:14 +0300

    fix

--- 5/nim2.py
+++ 5/nim2.py
@@ -1,53 +1,51 @@
 import random
 one = int(input('Исходное кол-во камней в первой куче: '))
 two = int(input('Исходное кол-во камней во второй куче: '))
-c = [1, 2, 3, 4, 5]
-d = [1, 2]
-while one != 0 and two != 0:
-    ii = random.choice(d)
-    iii = random.choice(c)
-    print(ii)
-    print(iii)
-
-    if a == 1 and one - b == 0 and two == 0:
-        print('Ты выйграл!!!')
-    elif a == 2 and two - b == 0 and one == 0:
-        print('Ты выйграл!!!')
+while one >= 0 or two >= 0:
+    if one == 0 and two == 0:
+        break
     else:
-        print('ИИ выйграл!!!')
-
-    if ii == 1:
-        if one - iii >= 0:
-            one -= iii
-    else:
-        if two - iii >= 0:
-            two -= iii
-    print(one, two)
-
-    a = int(input())
-    b = int(input())
-    while (a == 1 and b > one) or (a == 2 and b > two) or a != 1 or a != 2:
-        if a == 1 and b > one:
-            b = int(input('Введите число камней заново!'))
-        elif a == 2 and b > two:
-            b = int(input('Введите число камней заново!'))
-        else:
-            break
-
-    if a == 1 and one - b == 0 and two == 0:
-        print('Ты выйграл!!!')
-    elif a == 2 and two - b == 0 and one == 0:
-        print('Ты выйграл!!!')
-    else:
-        print('ИИ выйграл!!!')
-
-    if a == 1:
-        if one - b >= 0:
-            one -= b
-    else:
-        if two - b >= 0:
-            two -= b
-
-    print(one, two)
-
-print(0, 0)
+        if one != 0 and two != 0 or one == 0 and two != 0 or one != 0 and two == 0:
+            ii = random.randint(1, 10)
+            if one == 0:
+                a = 2
+            elif two == 0:
+                a = 1
+            else:
+                a = random.randint(1, 2)
+            comp = one
+            if a == 1:
+                b = one
+                one -= ii
+                if one <= 0:
+                    ii = b
+                    one = 0
+            elif a == 2:
+                count2_pile = one
+                two -= ii
+                if two <= 0:
+                    ii = count2_pile
+                    two = 0
+            if one <= 0 and two <= 0:
+                print('Компьютер забрал', comp, 'камней. Осталось',
+                      '0')
+                print('Компьютер победил!')
+                break
+            else:
+                print('Компьютер забрал', ii, 'камней из', a,
+                      'кучи. Осталось', one, two)
+                takes = int(input('Выберите кол-во камней: '))
+                you = int(input('Выберите кучу: '))
+                if you == 1:
+                    one -= takes
+                    print('Игрок забрал', takes, 'камней из', you,
+                          'кучи. Осталось', one, two)
+                elif you == 2:
+                    two -= takes
+                    print('Игрок забрал', takes, 'камней из', you,
+                          'кучи. Осталось', one, two)
+                if one <= 0 and two <= 0:
+                    print('Игрок забрал', takes, 'камней. Осталось',
+                          '0', '0')
+                    print('Игрок победил!')
+                    break
--- 5/nim3.py
+++ 5/nim3.py
@@ -2,20 +2,27 @@ import random
 one = int(input('Исходное кол-во камней в первой куче: '))
 two = int(input('Исходное кол-во камней во второй куче: '))
 three = int(input('Исходное кол-во камней в третьей куче: '))
+win = False # False - ходит бот, True - ходит игрок
 c = [1, 2, 3, 4, 5]
 d = [1, 2, 3]
 while one != 0 and two != 0 and three != 0:
+    # проверку чей ход
+    if win:
+        a = int(input('Введите номер кучи:'))
+        b = int(input('Введите количество камней:'))
+    else:
+        a = random.randint(b)
+
+
+
+    # ввод бота или игрока
+    # проверка на победу
+    # вывод оставшихся камней
+    # изменение вводящего
     ii = random.choice(d)
     iii = random.choice(c)
     print(ii)
     print(iii)
-
-    if ii == 1 and one - iii == 0 and two == 0 and three == 0:
-        print('ИИ выйграл!!!')
-    elif ii == 2 and two - iii == 0 and one == 0 and three == 0:
-        print('ИИ выйграл!!!')
-    elif ii == 3 and three - iii == 0 and one == 0 and two == 0:
-        print('ИИ выйграл!!!')
 
     if ii == 1:
         if one - iii >= 0:
@@ -26,11 +33,14 @@ while one != 0 and two != 0 and three != 0:
     else:
         if three - iii >= 0:
             three -= iii
-
+    if (win is False) and one == 0 and two == 0 and three == 0:
+        print('Бот выйграл!')
     print(one, two, three)
 
-    a = int(input())
-    b = int(input())
+    win = not win
+
+    a = int(input('Введите номер кучи: '))
+    b = int(input('Введите количество камней которое вы хотите взять: '))
     while (a == 1 and b > one) or (a == 2 and b > two) or (a == 3 and b > three) or a != 1 or a != 2:
         if a == 1 and b > one:
             b = int(input('Введите число камней заново!'))
@@ -39,22 +49,19 @@ while one != 0 and two != 0 and three != 0:
         elif a == 3 and b > three:
             b = int(input('Введите число камней заново!'))
 
-    if a == 1 and one - b == 0 and two == 0 and three == 0:
-        print('Ты выйграл!!!')
-    elif a == 2 and two - b == 0 and one == 0 and three == 0:
-        print('Ты выйграл!!!')
-    elif a == 3 and three - b == 0 and one == 0 and two == 0:
-        print('Ты выйграл!!!')
-
     if a == 1:
         if one - b >= 0:
             one -= b
     elif a == 2:
         if two - b >= 0:
             two -= b
-    else:
+    elif a == 3:
         if three - b >= 0:
             three -= b
 
+    if (win is True) and one == 0 and two == 0 and three == 0:
+        print('Игрок выйграл!')
+
     print(one, two, three)
+    win = not win
 print(0, 0, 0)
