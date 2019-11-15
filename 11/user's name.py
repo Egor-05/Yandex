43a37235099251c5a24720f8e3de1d3cca34ded4
egor <e.kulpin@mail.ru>
2019-11-15 20:56:14 +0300

    fix

--- 11/user's name.py
+++ 11/user's name.py
@@ -4,9 +4,9 @@ ns = 0
 for i in a:
     if ord('a') <= ord(i) >= ord('z'):
         s += 1
-    elif ord(i) == ord('_'):
+    elif i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 9:
         s += 1
-    elif ord('1') <= ord(i) >= ord('9') or ord('1') == ord(i):
+    elif ord(i) == ord('_'):
         s += 1
     else:
         ns = i
--- 11/vrednie sovety.py
+++ 11/vrednie sovety.py
@@ -1,7 +1,7 @@
 a = int(input())
 for i in range(a):
     ad = input()
-    if ad[:3] == 'Не ':
+    if ad[:3] == chr():
         print(ad[3:])
     else:
         print(ad)
