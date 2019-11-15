21b8ce804778b4aa541306173782c2d7b4f462bb
egor <e.kulpin@mail.ru>
2019-11-15 17:32:08 +0300

    fix

--- 9/odnofamilcy.py
+++ 9/odnofamilcy.py
@@ -1,9 +1,10 @@
-nw = set()
-a = int(input())
-s = 0
-for i in range(a):
-    names = input()
-    nw.add(names)
-for j in range(a):
-    if
-print(nw)
+nw = int(input())
+of = set()
+w = set()
+for i in range(nw):
+    now = input()
+    d = len(w)
+    w.add(now)
+    if d == len(w):
+        of.add(now)
+print(nw - len(w) + len(of))
