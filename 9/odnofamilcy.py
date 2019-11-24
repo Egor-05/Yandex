e309fc9f3c675a9397e01ae44bfa0cb5ddde5961
Егор <kulpin.ea@mail.ru>
2019-11-24 13:12:26 +0300

    '123'

--- 9/odnofamilcy.py
+++ 9/odnofamilcy.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-nw = set()
-a = int(input())
-s = 0
-for i in range(a):
-    names = input()
-    nw.add(names)
-for j in range(a):
-    if
-print(nw)
-=======
 nw = int(input())
 of = set()
 w = set()
@@ -19,4 +8,3 @@ for i in range(nw):
     if d == len(w):
         of.add(now)
 print(nw - len(w) + len(of))
->>>>>>> efd9a8d66c8a956ef6f0c0483298939b706250f8
