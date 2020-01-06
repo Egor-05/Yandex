0abf15d2b0f189d92463914f254a6cb82bdbf758
egor <e.kulpin@mail.ru>
2020-01-06 11:02:13 +0300

    fix

--- 16/bf.py
+++ 16/bf.py
@@ -1,3 +1,29 @@
+def bf(mest, command):
+    if command == '+':
+        if p[mest] == 255:
+            p[mest] = 0
+        else:
+            p[mest] += 1
+    elif command == '-':
+        if p[mest] == 0:
+            p[mest] = 255
+        else:
+            p[mest] -= 1
+    elif command == '>':
+        if mest == 29999:
+            mest = 0
+        else:
+            mest += 1
+    elif command == '<':
+        if mest == 0:
+            mest = 29999
+        else:
+            mest -= 1
+    elif command == '.':
+        print(p[mest])
+    return mest
+
+
 p = [0] * 30000
 a = input()
 m = 0
@@ -5,38 +31,24 @@ s = 0
 i = -1
 while 1:
     i += 1
-    if i >= len(a):
-        break
-    if a[i] == '+':
-        if p[m] == 255:
-            p[m] = 0
-        else:
-            p[m] += 1
-    elif a[i] == '-':
-        if p[m] == 0:
-            p[m] = 255
-        else:
-            p[m] -= 1
-    elif a[i] == '>':
-        if m == 29999:
-            m = 0
-        else:
-            m += 1
-    elif a[i] == '<':
-        if m == 0:
-            m = 29999
-        else:
-            m -= 1
-    elif a[i] == '.':
-        print(p[m])
-    elif a[i] == '[':
+    if a[i] == '[':
         if p[m] == 0:
             while 1:
                 i += 1
                 if a[i] == ']':
+                    i += 1
                     break
         else:
+            s = 0
             while 1:
+                s += 1
+                if a[i] == ']':
+                    break
+    m = bf(m, a[i])
+
+
+
+            while a[i] != ']':
                 i += 1
                 if a[i] == '+':
                     p[m] = 255
@@ -54,10 +66,8 @@ while 1:
                         m -= 1
                 elif a[i] == '.':
                     print(p[m])
-                elif a[i] == ']':
-                    break
                 elif a[i] == '[':
-                    while 1:
+                    while a[i] != ']':
                         i += 1
                         if a[i] == '+':
                             p[m] = 255
@@ -75,5 +85,3 @@ while 1:
                                 m -= 1
                         elif a[i] == '.':
                             print(p[m])
-                        elif a[i] == ']':
-                            break
