7d387bc0ddf95071ffbc63c5fd7400a3e1c727d0
egor <e.kulpin@mail.ru>
2020-02-17 21:27:32 +0300

    fix

--- o11.py
+++ o11.py
@@ -1,27 +1,26 @@
 import math
 
 
-def urav(k, b):
-    x = (2 * b) / (-2 * k)
+def urav(a1, c1, a2, c2):
+    b1 = b2 = 1
+    x = (c1 * b2 - c2 * b1) / (b1 * a2 - b2 * a1)
+    y = (c2 * a1 - c1 * a2) / (b1 * a2 - b2 * a1)
+    return x, y
 
 
 def equation(first_point, second_point, kb):
     first_pointx, first_pointy = [float(i) for i in first_point.split(";")]
     second_pointx, second_pointy = [float(i) for i in second_point.split(";")]
-    k = 0
+    a = 0
     if first_pointx - second_pointx != 0:
-        k = (first_pointy - second_pointy) / (first_pointx - second_pointx)
-    b = first_pointy - k * first_pointx
-    if kb == 'k':
-        return k
-    elif kb == 'b':
-        return b
+        a = (first_pointy - second_pointy) / (first_pointx - second_pointx)
+    c = first_pointy - a * first_pointx
+    if kb == 'a':
+        return a
+    elif kb == 'c':
+        return c
     else:
-        return [k, b]
-
-
-def equation1(x1, y1, x2, y2):
-
+        return [a, c]
 
 
 b_x, b_y, c_x, c_y, d_x, d_y = [int(i) for i in input().split()]
@@ -29,4 +28,4 @@ u = 180 - (math.atan(equation('0;0', f'{b_x};{b_y}', 'k')) * 180 / math.pi
            + math.atan(equation(f'{c_x};{c_y}', f'{d_x};{d_y}', 'k') * 180 / math.pi))
 k1, b1 = equation('0;0', f'{b_x};{b_y}', 'kb')
 k2, b2 = equation(f'{c_x};{c_y}', f'{d_x};{d_y}', 'kb')
-print(urav(k1, b1))
+print(urav(k1, b1, k2, b2))
