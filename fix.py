fb26a38a47c4a34ae74bbc38558455ad1ff07e11
Егор <kulpin.ea@mail.ru>
2020-03-16 19:02:08 +0300

    from NB

--- fix.py
+++ fix.py
@@ -1,12 +1,21 @@
-import pymorphy2
+import xlsxwriter
+import sys
 
 
-morph = pymorphy2.MorphAnalyzer()
-word = morph.parse('бутылка')[0]
-for i in range(99, 0, -1):
-    print(f'В холодильнике {i} {word.make_agree_with_number(i).word} кваса.')
-    print('Возьмём одну и выпьем.')
-    if (i - 1) % 10 == 1 and i - 1 != 11:
-        print(f'Осталась {i - 1} {word.make_agree_with_number(i - 1).word} кваса.')
-    else:
-        print(f'Осталось {i - 1} {word.make_agree_with_number(i - 1).word} кваса.')
+def export_check(text):
+    workbook = xlsxwriter.Workbook('res.xlsx')
+    worksheet = workbook.add_worksheet()
+    text = text.strip('\n').split('\n')
+    for i in range(len(text)):
+        worksheet.write(i, 0, (text[i].split('\t'))[0])
+        worksheet.write(i, 1, float((text[i].split('\t'))[1]))
+        worksheet.write(i, 2, int((text[i].split('\t'))[2]))
+        x = f'=B{i + 1}*C{i + 1}'
+        worksheet.write(i, 3, x)
+    x = '=SUM(' + 'D' + '1' + ':' + 'D' + str(len(text)) + ')'
+    worksheet.write(len(text), 0, 'Итого')
+    worksheet.write(len(text), 3, x)
+    workbook.close()
+
+
+export_check(sys.stdin.read())
