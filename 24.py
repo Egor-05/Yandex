12626851315d81eeaa95be44cbbf1a5d27fbb2e0
egor <e.kulpin@mail.ru>
2020-04-14 15:57:38 +0300

    fromHP

--- 24.py
+++ 24.py
@@ -2,36 +2,30 @@ from sys import stdin
 from collections import defaultdict
 
 
-def way(p_now, arr, s):
-    minn = 10 ** 10
+def way(p_now, arr):
+    arr.append(p_now)
     for i in from_to[p_now]:
-        if i in arr:
-            continue
         if i == end:
-            return s
-        if p_now == start:
-            arr = []
-            s = 0
-        arr.append(i)
-        w = way(i, arr, s)
-        if w < minn:
-            minn = w
-            s += w
-    return s
+            global ways
+            arr1 = arr + [i]
+            ways[sum([from_to[arr1[j]][arr1[j + 1]] for j in range(len(arr1) - 1)])] = ', '.join(arr1)
+        elif i in arr:
+            continue
+        else:
+            way(i, arr.copy())
 
 
-from_to = defaultdict(list)
-len_way = {}
+from_to = defaultdict(dict)
+ways = {}
 start = 0
 end = 0
 for i in stdin:
     i.strip('\n')
     a = i.split()
     if len(a) == 3:
-        from_to[a[0]].append(a[1])
-        from_to[a[1]].append(a[0])
-        len_way[f'{a[0]}-{a[1]}'] = a[2]
-        len_way[f'{a[1]}-{a[1]}'] = a[2]
+        from_to[a[0]][a[1]] = int(a[2])
+        from_to[a[1]][a[0]] = int(a[2])
     else:
         start, end = a
-print(way(start, [], 0))
+way(start, [])
+print(ways[min(list(ways.keys()))])
--- 26.py
+++ 26.py
@@ -1,12 +1,1 @@
-import sys
-lst = []
-for i in sys.stdin:
-    if '\n' in i:
-        i = i[:-1]
-    i = i.split(' ')
-    lst.append([int(j) for j in i if j.isdigit()])
-res = sum(lst[0])
-if all([sum(x) == res for x in lst]) and all([sum(x) == res for x in list(zip(*lst))]):
-    print('YES')
-else:
-    print('NO')
+print(((-20 + 7) ** 2 + 5) + (36 + 6))
